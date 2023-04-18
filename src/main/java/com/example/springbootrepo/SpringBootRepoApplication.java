972d67d85046be7544b6a6583bad2576dda053de
gaoxiang.huang <hgx1233626@163.com>
2023-04-18 10:56:54 +0800

    fix

--- src/main/java/com/example/springbootrepo/SpringBootRepoApplication.java
+++ src/main/java/com/example/springbootrepo/SpringBootRepoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.springbootrepo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
+public class SpringBootRepoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRepoApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
--- src/test/java/com/example/springbootrepo/SpringBootRepoApplicationTests.java
+++ src/test/java/com/example/springbootrepo/SpringBootRepoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.springbootrepo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringBootRepoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
