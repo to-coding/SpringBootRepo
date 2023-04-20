8e77c9b0a0080bb69720be1e15a34a1352472d44
gaoxiang.huang <hgx1233626@163.com>
2023-04-20 16:08:06 +0800

    test

--- src/main/java/com/example/springbootrepo/controller/TestController.java
+++ src/main/java/com/example/springbootrepo/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("test")
     String test(){
-        return "Ok";
+        return "Ok test";
     }
+
 }
