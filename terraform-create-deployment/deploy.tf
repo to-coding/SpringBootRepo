901cbf477a1959529e908a36fff365b3d895ab03
Shuanghui SH1 Bao <baosh1@lenovo.com>
2023-04-20 17:11:57 +0800

    update workflow

--- terraform-create-deployment/deploy.tf
+++ terraform-create-deployment/deploy.tf
@@ -22,7 +22,6 @@ data "aws_eks_cluster" "cluster" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  load_config_file       = false
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
