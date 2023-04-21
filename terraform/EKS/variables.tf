a6760c4df1e50e2d5c215765880f0b8224a8a045
Yingqi YQ12 Huang <huangyq12@lenovo.com>
2023-04-21 11:05:23 +0800

    feat: add ecr

--- terraform/EKS/variables.tf
+++ terraform/EKS/variables.tf
@@ -3,3 +3,26 @@ variable "region" {
   type        = string
   default     = "ap-southeast-2"
 }
+variable "ecr_name" {
+  description = "name of the ECR registry"
+  type        = string
+  default     = "beach-hyq"
+}
+
+variable "image_mutability" {
+  description = "provide image mutability"
+  type        = string
+  default     = "MUTABLE"
+}
+
+variable "encrypt_type" {
+  description = "provide type of encryption here"
+  type        = string
+  default     = "KMS"
+}
+
+variable "tags" {
+  description = "the key-value maps for tagging"
+  type        = map(string)
+  default     = {}
+}
