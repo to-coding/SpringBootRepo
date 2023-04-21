2d71323222e214c99c2ff7a31e7efbdce63a5f5c
Yingqi YQ12 Huang <huangyq12@lenovo.com>
2023-04-21 15:49:00 +0800

    feat：deploy

---
+++ terraform/DEPLOYMENT/terraform.tf
@@ -0,0 +1,35 @@
+terraform {
+
+  cloud {
+    organization = "hyq"
+
+    workspaces {
+      name = "hyq-depolyment"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.47.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
+
