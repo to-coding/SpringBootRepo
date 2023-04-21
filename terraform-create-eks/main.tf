2ba4d56264ef84c59167c45159c99e8fc69932c6
Shuanghui SH1 Bao <baosh1@lenovo.com>
2023-04-21 10:21:19 +0800

    update clustername

--- terraform-create-eks/main.tf
+++ terraform-create-eks/main.tf
@@ -8,13 +8,13 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "new-education-eks-${random_string.suffix.result}"
+  cluster_name = "new-education-eks"
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
+#resource "random_string" "suffix" {
+#  length  = 8
+#  special = false
+#}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
